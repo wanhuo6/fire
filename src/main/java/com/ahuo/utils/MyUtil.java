5a5f78d63a51c1e9f53af0e498bddd4f7a2ba3ed
liuhuijie <edward_hs@163.com>
2017-05-22 17:14:35 +0800

    lib

---
+++ src/main/java/com/ahuo/utils/MyUtil.java
@@ -0,0 +1,11 @@
+package com.ahuo.utils;
+
+/**
+ * Created by ahuo on 17-5-22.
+ */
+public class MyUtil {
+
+    public static String getName(String source){
+        return source+"...";
+    }
+}
